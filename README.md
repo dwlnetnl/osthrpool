5275fa0156ea8609470da4134e755d8aa0822a6d
Anner van Hardenbroek <anner@eigengang.nl>
2015-09-27 20:55:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# osthrpool
+Package osthrpool provides a pool of locked OS threads that grows and shrinks automatically and distribute the load over the threads.
+
+Example:
+```go
+	// Make a new pool of 5 goroutines that are locked to an OS thread.
+	// These goroutines are automatically unlocked after 500ms if there
+	// are no more tasks to execute.
+	p := osthrpool.New(5, 500*time.Millisecond)
+
+	// Execute 10 tasks.
+	for i := 0; i < 10; i++ {
+		fmt.Println("index:", i)
+
+		p.Execute(func() {
+			fmt.Println("task: ", i)
+		})
+	}
+
+	// Output:
+	// index: 0
+	// task:  0
+	// index: 1
+	// task:  1
+	// index: 2
+	// task:  2
+	// index: 3
+	// task:  3
+	// index: 4
+	// task:  4
+	// index: 5
+	// task:  5
+	// index: 6
+	// task:  6
+	// index: 7
+	// task:  7
+	// index: 8
+	// task:  8
+	// index: 9
+	// task:  9
+```
---
+++ osthrpool.go
@@ -0,0 +1,165 @@
+// Package osthrpool provides a pool of locked OS threads.
+package osthrpool
+
+import (
+	"container/heap"
+	"runtime"
+	"sync"
+	"time"
+)
+
+// Task represents a task that requires to be run on a locked OS thread.
+type Task func()
+
+// Pool represents a pool of locked OS threads.
+type Pool struct {
+	maxSize int
+	timeout time.Duration
+	workers queue
+	mu      sync.Mutex // protects workers
+	exit    chan *worker
+}
+
+// New returns a pool of locked OS threads that grows to it's maximum size and
+// shrinks automatically depending on the load. A thread automatically unlocks
+// itself after a timeout if there are no more tasks to process.
+func New(maxSize int, timeout time.Duration) *Pool {
+	return &Pool{
+		maxSize: maxSize,
+		timeout: timeout,
+		workers: make(queue, 0, maxSize),
+		exit:    make(chan *worker, maxSize),
+	}
+}
+
+// Execute executes the given task on a locked OS thread.
+func (p *Pool) Execute(t Task) {
+	p.mu.Lock()
+	p.startWorker()
+	w := p.getWorker()
+	p.mu.Unlock()
+
+	w.tasks <- t // send task for execution
+	<-w.done     // wait on result of task
+
+	p.putWorker(w)
+}
+
+// startWorker starts a worker if the pool has not reached it's maximum number
+// of workers.
+func (p *Pool) startWorker() {
+	if p.workers.Len() < int(p.maxSize) {
+		if p.workers.Len() == 0 {
+			// Start background process that collects terminated workers and removes
+			// them from the pool.
+			go p.collectWorkers()
+		}
+
+		w := newWorker(p.exit, p.timeout)
+		heap.Push(&p.workers, w)
+		go w.work()
+	}
+}
+
+// collectWorkers is a background process that remove terminated workers from
+// the worker pool. This stops itself if there are no more active workers left.
+func (p *Pool) collectWorkers() {
+	for {
+		select {
+		case w := <-p.exit:
+			p.mu.Lock()
+
+			heap.Remove(&p.workers, w.index)
+
+			if p.workers.Len() == 0 {
+				p.mu.Unlock()
+				return
+			}
+
+			p.mu.Unlock()
+		}
+	}
+}
+
+// getWorker returns the lightest loaded worker from the worker pool.
+func (p *Pool) getWorker() *worker {
+	w := heap.Pop(&p.workers).(*worker)
+	w.pending++
+	// Store worker back into queue to adjust the load of the worker.
+	heap.Push(&p.workers, w)
+	return w
+}
+
+// putWorker puts a worker back in the worker pool.
+func (p *Pool) putWorker(w *worker) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	w.pending--
+	// Reorder the queue based on the load of the workers.
+	heap.Fix(&p.workers, w.index)
+}
+
+type worker struct {
+	timeout time.Duration
+	exit    chan *worker
+	tasks   chan Task
+	done    chan struct{}
+	index   int
+	pending int
+}
+
+func newWorker(exit chan *worker, timeout time.Duration) *worker {
+	return &worker{
+		exit:    exit,
+		timeout: timeout,
+		tasks:   make(chan Task),
+		done:    make(chan struct{}),
+	}
+}
+
+func (w *worker) work() {
+	runtime.LockOSThread()
+
+	for {
+		select {
+		case task := <-w.tasks:
+			task()
+			w.done <- struct{}{}
+		case <-time.After(w.timeout):
+			runtime.UnlockOSThread()
+			w.exit <- w
+			return
+		}
+	}
+}
+
+// queue is a priority queue of workers with the lightest loaded worker as head
+// of the queue.
+type queue []*worker
+
+func (q queue) Len() int           { return len(q) }
+func (q queue) Less(i, j int) bool { return q[i].pending < q[j].pending }
+
+func (q queue) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+	q[i].index = i
+	q[j].index = j
+}
+
+func (q *queue) Push(item interface{}) {
+	s := *q
+	n := len(s)
+	s = s[0 : n+1]
+	w := item.(*worker)
+	s[n] = w
+	w.index = n
+	*q = s
+}
+
+func (q *queue) Pop() interface{} {
+	s := *q
+	*q = s[0 : len(s)-1]
+	w := s[len(s)-1]
+	w.index = -1 // for safety
+	return w
+}
